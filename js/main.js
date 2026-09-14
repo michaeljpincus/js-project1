b313ae285588efa10d2a10cf3e318149e914da4f
michaeljpincus <michaeljpincus@gmail.com>
2026-09-13 23:10:00 -0400

    Js edit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Access token configured only for github url requests 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWpwanBnIiwiYSI6ImNtdTBlODVjeTB5bm4yenBtcW15Y3dmbzgifQ.Gyy9P1sfCGT2QtizeNiqgw'
 
 const map = new mapboxgl.Map({
